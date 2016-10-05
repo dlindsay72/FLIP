befa26fdb913c6aa92598550d468f7a5bf76e6f5
Dan Lindsay <dlindsay72@hotmail.com>
2016-10-05 19:57:28 -0400

    Board and stone starting layout done

--- FLIP/GameScene.swift
+++ FLIP/GameScene.swift
@@ -11,11 +11,50 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var rows = [[Stone]]()
+    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
         
+        let background = SKSpriteNode(imageNamed: "background")
+        
+        background.blendMode = .replace
+        background.zPosition = 1
+        addChild(background)
+        
+        let gameBoard = SKSpriteNode(imageNamed: "board")
+        
+        gameBoard.name = "board"
+        gameBoard.zPosition = 2
+        addChild(gameBoard)
+        
+        let offsetX = -280
+        let offsetY = -281
+        let stoneSize = 80
+        
+        for row in 0..<8 {
+            var colArray = [Stone]()
+            
+            for col in 0..<8 {
+                let stone = Stone(color: UIColor.clear, size: CGSize(width: stoneSize, height: stoneSize))
+                
+                stone.position = CGPoint(x: offsetX + (col * stoneSize), y: offsetY + (row * stoneSize))
+                
+                stone.row = row
+                stone.col = col
+                
+                gameBoard.addChild(stone)
+                colArray.append(stone)
+            }
+            rows.append(colArray)
+        }
+        
+        rows[4][3].setPlayer(.white)
+        rows[4][4].setPlayer(.black)
+        rows[3][4].setPlayer(.white)
+        rows[3][3].setPlayer(.black)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
---
+++ FLIP/StoneColor.swift
@@ -0,0 +1,14 @@
+//
+//  StoneColor.swift
+//  FLIP
+//
+//  Created by Dan Lindsay on 2016-10-05.
+//  Copyright © 2016 Dan Lindsay. All rights reserved.
+//
+
+import Foundation
+
+
+enum StoneColor: Int {
+    case empty, choice, white, black
+}
